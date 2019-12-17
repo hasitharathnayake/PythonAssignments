6c7e8a36cd57df2f64ef81d6908ad3914cd8e319
Hasitha Rathnayake <42506291+hasitharathnayake@users.noreply.github.com>
2019-12-17 01:07:27 -0500

    Update Assignment_8.5.py

--- Python_Data_Structures_assignments/Assignment_8.5.py
+++ Python_Data_Structures_assignments/Assignment_8.5.py
@@ -1,7 +1,4 @@
-#8.5 Open the file mbox-short.txt and read it line by line. When you find a line that starts with 'From ' like the following line:
-#You will parse the From line using split() and print out the second word in the line
-#(i.e. the entire address of the person who sent the message). Then print out a count at the end.
-#Hint: make sure not to include the lines that start with 'From:'.
+#This program finds the keyword 'From' and then strips out email addresses from a textfile.
 
 file_name=input("Enter File Name/Full Path: ")
 try:
