ef537cb4ae3f7aa6e570f4d3f85360fa61805b01
Hasith Rathnayake <hasithrathnayake@Hasiths-MacBook-Pro.local>
2020-01-04 01:13:45 -0500

    Ex 1 final for week 2

--- Using_Databases_with_Python/exercise1UsingSQLITE.py
+++ Using_Databases_with_Python/exercise1UsingSQLITE.py
@@ -1,2 +1,32 @@
 import sqlite3
-connection=sqlite3.connect('Ages')
+
+connection=sqlite3.connect('Ages.sqlite')
+cur=connection.cursor()
+
+#if the db exists then override
+#then create a table and 2 columns name and age
+cur.execute('DROP TABLE IF EXISTS Ages')
+cur.execute('CREATE TABLE Ages (name VARCHAR(128), age INTEGER)')
+
+#Entering data
+data='''DELETE FROM Ages;INSERT INTO Ages (name, age) VALUES ('Naomie', 13);
+INSERT INTO Ages (name, age) VALUES ('Raonaid', 20);
+INSERT INTO Ages (name, age) VALUES ('Kellsey', 20);
+INSERT INTO Ages (name, age) VALUES ('Youer', 22);
+INSERT INTO Ages (name, age) VALUES ('Elody', 25);'''
+
+sqlcmds=data.split(';')
+cleanSqlcmds=list()
+for item in sqlcmds[1:-1]:
+    item=item.strip('\n')
+    cur.execute(item)
+#commit to disk
+connection.commit()
+
+#view db
+for row in cur.execute('SELECT name, age FROM Ages ORDER BY age'):
+    print(str(row[0]), row[1])
+#for getting marks
+for row in cur.execute('SELECT hex(name||age) AS X FROM Ages ORDER BY X'):
+    print(str(row[0]))
+    break #print just one
